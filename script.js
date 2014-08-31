1c0fbe74f59d037936d8925c3ffb482e2420a0e3
Clarkok <mail@clarkok.com>
2014-08-31 15:23:20 +0800

    vmin fix

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 "use strict";
 (function (w, $) {
+  console.log($('.item').css('top'));
+})(window, window.jQuery);
+
+(function (w, $) {
+  var Notifier = function () {
+    this.target = $('#notice');
+    this.context = this.target.find('#notice-content');
+    this.target.removeClass('show');
+    this.r = 0;
+    this.target.on('click', function () {
+      $(this).removeClass('show error');
+    });
+  };
+
+  Notifier.prototype.notice = function (txt) {
+    w.clearTimeout(this.r);
+    var _this = this;
+    this.context.html(txt);
+    this.target.addClass('show');
+    this.target.removeClass('error');
+    this.r = w.setTimeout(function () {
+      _this.target.removeClass('show');
+    }, 5000);
+  };
+
+  Notifier.prototype.error = function (txt) {
+    w.clearTimeout(this.r);
+    var _this = this;
+    this.context.html(txt);
+    this.target.addClass('show error');
+    this.r = w.setTimeout(function () {
+      _this.target.removeClass('show error');
+    }, 5000);
+  };
+
+  w.Notifier = Notifier;
+})(window, window.jQuery);
+(function (w, $) {
   $('input[type=text]').on('focus', function () {
     $(this).parents('.input').addClass('focus');
   }).on('blur', function () {
@@ -19,6 +57,40 @@
   });
   $('select').on('click', function (e) {
     e.stopPropagation();
+  });
+
+  var notifier = new w.Notifier();
+
+  $('#joinus-wrapper').on('submit', function (e) {
+    e.preventDefault();
+    w.location.hash = '#intro';
+    $('.error').removeClass('error');
+    console.log($(this).attr('action'));
+    $.post($(this).attr('action'), $(this).serialize(), function (data) {
+      if (data.code == 1) {
+        notifier.error('提交失败，请稍后重试');
+      }
+      else if (data.code == 2) {
+        notifier.error('表单中有错误，<a href="#joinus">点击查看</a>');
+        var l = data.error_list.length;
+        for (var i = 0; i < l; ++i) {
+          $('#input-' + data.error_list[i]).addClass('error');
+        }
+      }
+      else if (data.code == -1) {
+        w.location.hash = '#detail-tech';
+        notifier.notice('年轻人有前途，快加入技术研发中心吧');
+      }
+      else {
+        notifier.notice('提交成功');
+        $('.error').removeClass('error');
+      }
+    }, 'json');
+  });
+
+  $('.button').on('click', function () {
+    console.log('submit');
+    $(this).parents('form').trigger('submit');
   });
 
   if (w.location.hash.length <= 1) {
